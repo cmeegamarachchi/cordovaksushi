6404d4b7830c40aa22a3b3036ef080813744aa6d
Chintana Meegamarachchi <chintana@appsolve.com.au>
2015-07-14 21:29:50 +0800

    Implement menu page
    Implement menu page by implementing view and controller
    Update index page only to display featured items

--- CordovaKSushi/app/controllers.js
+++ CordovaKSushi/app/controllers.js
@@ -4,8 +4,27 @@
         var self = this;
 
         self.sushiCart = sushiCartService;
+
+        self.filterFeatured = function () {
+            self.sushiCart.productsDataSource.group([]);
+            self.sushiCart.productsDataSource.filter({ field: "featured", operator: "eq", value: true });
+        }
     }
 
     app.controller('indexController', ['sushiCartService', indexController]);
+
+
+    function menuController(sushiCartService) {
+        var self = this;
+
+        self.sushiCart = sushiCartService;
+
+        self.groupByCategory = function () {
+            self.sushiCart.productsDataSource.filter([]);
+            self.sushiCart.productsDataSource.group({ field: "category" });
+        }
+    }
+
+    app.controller('menuController', ['sushiCartService', menuController]);
 
 })(angular.module('sushiApp'));
