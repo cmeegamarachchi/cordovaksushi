88756f5fb687a1a41b2e23f0548e88cc8a4daa80
Chintana Meegamarachchi <chintana@appsolve.com.au>
2015-08-13 10:32:29 +0800

    Add aHrefSanitizationWhitelist to application

--- CordovaKSushi/app/app.js
+++ CordovaKSushi/app/app.js
@@ -2,6 +2,13 @@
 
     var app = angular.module('sushiApp', ['kendo.directives']);
 
+    app.config([
+        '$compileProvider',
+        function ($compileProvider) {
+            $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|http|local|ftp|mailto|chrome-extension):/);
+        }
+    ]);
+
     function initService(sushiCartService) {
         sushiCartService.init();
     }
